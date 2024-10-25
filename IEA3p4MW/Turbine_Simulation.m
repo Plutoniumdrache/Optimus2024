b41ad6341447b9b9fcaabe73da2a0527930adc6c
Felix <felix.lehmann@stud.hs-flensburg.de>
2024-10-25 13:42:50 +0200

    add pulse control via matlab file
    also change a simulation out name to solve some version issues

--- IEA3p4MW/Turbine_Simulation.m
+++ IEA3p4MW/Turbine_Simulation.m
@@ -8,15 +8,25 @@ clearvars;close all;clc;
 %% PreProcessing SLOW for all simulations
 
 % Default Parameter Turbine and Controller
-Parameter                       = DefaultParameter_SLOW2DOF;
-Parameter                       = DefaultParameter_PitchActuator(Parameter);
-Parameter                       = DefaultParameter_FBv1_ADv14(Parameter);
+Parameter                           = DefaultParameter_SLOW2DOF;
+Parameter                           = DefaultParameter_PitchActuator(Parameter);
+Parameter                           = DefaultParameter_FBv1_ADv14(Parameter);
 
 % Time
-dt                              = 1/80;
-Parameter.Time.dt               = dt;   % [s] simulation time step              
-Parameter.Time.TMax             = 60;   % [s] simulation length
+dt                                  = 1/80;
+Parameter.Time.dt                   = dt;   % [s] simulation time step              
+Parameter.Time.TMax                 = 60;   % [s] simulation length
 
+% Tourque offset
+Parameter.Offset.Torque             = 0.1;  % [-] Fraction of nominal Tourque
+% positive pulses
+Parameter.Offset.positive.period    = 30;   % [s] Period of the pulse
+Parameter.Offset.positive.width     = 5;    % [%] Width of the pulse (100 = period) 
+Parameter.Offset.positive.delay     = 15;   % [s] dely against 0 sec
+% negative pulses
+Parameter.Offset.negative.period    = 30;   % [s] Period of the pulse
+Parameter.Offset.negative.width     = 5;    % [%] Width of the pulse (100 = period) 
+Parameter.Offset.negative.delay     = 30;   % [s] dely against 0 sec
 %% Loop over Operation Points
 
 OPs = [12 16 20 24];
@@ -39,12 +49,12 @@ for iOP=1:nOP
     Parameter.IC.x_T          	    = interp1(SteadyStates.v_0,SteadyStates.x_T,  OP,'linear','extrap');
 
     % Processing SLOW for this OP
-    sim('FBv1_SLOW2DOF.mdl')
+    sim_out = sim('FBv1_SLOW2DOF.mdl');
     
     % collect simulation Data
-    Omega(:,iOP) = logsout.get('y').Values.Omega.Data;
-    Power_el(:,iOP) = logsout.get('y').Values.P_el.Data;
-    Torque(:,iOP) = logsout.get('y').Values.M_g.Data;
+    Omega(:,iOP) = sim_out.logsout.get('y').Values.Omega.Data;
+    Power_el(:,iOP) = sim_out.logsout.get('y').Values.P_el.Data;
+    Torque(:,iOP) = sim_out.logsout.get('y').Values.M_g.Data;
 end
 
 
@@ -53,18 +63,18 @@ figure
 
 subplot(311)
 hold on;box on;grid on;
-plot(tout,Omega*60/2/pi)
+plot(sim_out.tout,Omega*60/2/pi)
 ylabel('\Omega [rpm]')
 legend(strcat(num2str(OPs'),' m/s'))
 
 subplot(312)
 hold on;box on;grid on;
-plot(tout,Power_el./1000)
+plot(sim_out.tout,Power_el./1000)
 ylabel('Power [kW]')
 legend(strcat(num2str(OPs'),' m/s')) 
 
 subplot(313)
 hold on;box on;grid on;
-plot(tout,Torque)
+plot(sim_out.tout,Torque)
 ylabel('Torque [Nm]')
 legend(strcat(num2str(OPs'),' m/s')) 
